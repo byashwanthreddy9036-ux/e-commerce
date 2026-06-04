e5bdf3df3ba13c32536399b6209baaa699e8262b
byashwanthreddy9036-ux <byashwanthreddy9036@gmail.com>
2026-06-04 13:42:00 +0530

    tailwind setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
