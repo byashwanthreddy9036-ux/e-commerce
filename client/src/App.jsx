b798a1c3e8af58b5771407ed9db242a05e58e845
byashwanthreddy9036-ux <byashwanthreddy9036@gmail.com>
2026-06-26 01:54:01 +0530

    app routing and protected route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,35 @@
-// import React from 'react'
-// import Register from './pages/Register'
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
+import Wishlist from './pages/Wishlist'
+import Profile from './pages/Profile'
+import AdminLogin from './pages/admin/AdminLogin'
+import AdminProducts from './pages/admin/AdminProducts'
 
-// const App = () => {
-//   return (
-//     <>
-//       <Router>
-//         <Routes>
-//           {/* <Route path="/register" element={<Register />} /> */}
-//           <Route path="*" element={<Navigate to="/login" />} />
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/admin/login' element={<AdminLogin />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetail />} />
+          <Route path='/cart' element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path='/wishlist' element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
+          <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path='/admin/products' element={<ProtectedRoute adminOnly><AdminProducts /></ProtectedRoute>} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  )
+}
 
-//         </Routes>
-//       </Router>
-//       {/* <Router>
-//         <Routes>
-//           <Route path="/register" element={<Register />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/home" element={<Home />} />
-//           <Route path="/profile" element={<Profile />} />
-//           <Route path="/my-urls" element={<MyUrls />} />
-//           <Route path="*" element={<Navigate to="/login" />} />
-//         </Routes>
-//       </Router> */}
-//     </>
-//   )
-// }
-
-// export default App
+export default App
